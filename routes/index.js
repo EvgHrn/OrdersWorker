aaa1ab5de7b74f39450376e4b84f1435ea0f7fc9
EvgHrn <evg.hrn@gmail.com>
2020-09-24 08:33:42 +0400

    Get order data from ftp

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Ftp = require('../utils/ftp');
+
+router.get('/getOrders/:days', async (req, res, next) => {
+  const filesList = await Ftp.getFilesList(req.params.days);
+  res.send(filesList.map((fileObj) => fileObj.name));
+});
+
+router.get('/getOrderData/:orderNumber', async (req, res, next) => {
+  const fileString = await Ftp.readFileFromFtp(req, res, req.params.orderNumber);
+  res.send(fileString);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
---
+++ utils/ftp.js
@@ -0,0 +1,123 @@
+// const hash = require('object-hash');
+const sub = require('date-fns/sub');
+const isBefore = require('date-fns/isBefore');
+const parse = require('date-fns/parse');
+const PromiseFtp = require('promise-ftp');
+const ftp = require("basic-ftp");
+const fs = require('fs');
+
+const ftpPromise = new PromiseFtp();
+
+class Ftp {
+
+    static getFilesList = async (days) => {
+        const client = new ftp.Client();
+        client.ftp.verbose = true;
+        const startDate = sub(new Date(), { days: days });
+        try {
+            await client.access({
+                host: process.env.FTP_HOST,
+                user: process.env.FTP_USER,
+                password: process.env.FTP_PASSWORD,
+                secure: false
+            });
+            // console.log(await client.list('change/access'));
+            const fileList = await client.list('change/access');
+            client.close();
+            const filteredFileList = fileList.filter((fileObj) => isBefore(startDate, parse(fileObj.rawModifiedAt, 'MM-dd-yy hh:mmaa', new Date())));
+            return  filteredFileList;
+        }
+        catch(err) {
+            console.log(err);
+            client.close();
+            return false;
+        }
+    }
+
+    static readFileFromFtp = async (req, res, fileName) => {
+        const ftp = ftpPromise;
+        try {
+            await ftp.connect({
+                    host: process.env.FTP_HOST,
+                    user: process.env.FTP_USER,
+                    password: process.env.FTP_PASSWORD
+                });
+            const stream = await ftp.get(`change/access/${fileName}`);
+            await new Promise((resolve, reject) => {
+                res.on('finish', resolve);
+                stream.once('error', reject);
+                stream.pipe(res);
+            });
+        } catch(e) {
+            console.error(e);
+        } finally {
+            await ftp.end();
+        }
+    }
+
+    static getFileFromFtp = async (fileName) => {
+        const client = new ftp.Client();
+        client.ftp.verbose = true;
+        try {
+            await client.access({
+                host: process.env.FTP_HOST,
+                user: process.env.FTP_USER,
+                password: process.env.FTP_PASSWORD,
+                secure: true
+            });
+            console.log(await client.list());
+            await client.downloadTo(fileName, fileName);
+            client.close();
+            return fs.readFileSync(fileName, 'utf8');
+        }
+        catch(err) {
+            console.log(err);
+            client.close();
+            return false;
+        }
+    }
+
+    static uploadFileToFtp = async (localPathFileName, remoteFileName) => {
+        const client = new ftp.Client();
+        client.ftp.verbose = true;
+        try {
+            await client.access({
+                host: process.env.FTP_HOST,
+                user: process.env.FTP_USER,
+                password: process.env.FTP_PASSWORD,
+                secure: false
+            });
+            console.log(await client.list());
+            await client.uploadFrom(localPathFileName, remoteFileName);
+            client.close();
+            return true;
+        }
+        catch(err) {
+            console.log(err);
+            client.close();
+            return false;
+        }
+    }
+
+    static uploadDataFile = async (fileName) => {
+        const data = await Db.getAllTours();
+        const tours = data.map((tour) => applyDurationsToActivitiesBlocksForDashboard(tour._doc));
+        console.log("Getting tours complete");
+        // const aloneActivitiesBlocks = await Db.getAloneActivitiesBlocks();
+        // console.log("Getting aloneActivitiesBlocks complete");
+        const excelFileResult = await this.createExcel(fileName, tours, []);
+        if (excelFileResult) {
+            console.log("Excel created");
+            const uploadingResult = await this.uploadFileToFtp(fileName, `/change/access/${fileName}`);
+            if(uploadingResult) {
+                console.log("Excel file uploaded");
+            } else {
+                console.log("Excel file uploading error");
+            }
+        } else {
+            console.log("Excel creating error");
+        }
+    }
+}
+
+module.exports = Ftp;
