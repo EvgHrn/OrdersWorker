f818487175a09927e973eeaf7dd87b50c1f4a364
EvgHrn <evg.hrn@gmail.com>
2020-10-08 12:39:42 +0400

    Get orders list

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Ftp = require('../utils/ftp');
 
-router.get('/getOrders/:days', async (req, res, next) => {
-  const filesList = await Ftp.getFilesList(req.params.days);
-  res.send(filesList.map((fileObj) => fileObj.name));
+router.get('/getOrdersNumbersListByPeriod', async (req, res, next) => {
+
+  if(req.query.st !== process.env.SECRET) {
+    res.status(401).send('Unauth');
+    return;
+  }
+
+  const list = await Ftp.getFilesList(req.query.periodDays);
+  //
+  console.log("Gonna send orders list from ftp");
+  res.send(list);
+
 });
 
 router.get('/getOrderData', async (req, res, next) => {
